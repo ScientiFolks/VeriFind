8b9aa308a268a3b7ed14e62df48bc7d068eb4670
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2024-11-23 12:16:36 +0700

    feat: add color

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black-fig': '#2B2C33',
+        'gray-1': '#515151',
+        'gray-2': '#F0F0F0',
+        'gradient-1': '#F66A4B',
+        'gradient-2': '#A24AE7',
+        'gradient-3': '#4859F3',
+        'yellow-fig': '#F19E38'
+      }
+    },
   },
   plugins: [],
 }
