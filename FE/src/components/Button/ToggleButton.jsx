aed8d28e5b15cc9eba58fc70cebb7e55aff51aec
Ghazi Akmal Fauzan <13521058@std.stei.itb.ac.id>
2024-11-23 16:20:11 +0700

    fix: toggle button order

--- FE/src/components/Button/ToggleButton.jsx
+++ FE/src/components/Button/ToggleButton.jsx
@@ -12,16 +12,6 @@ const ToggleButton = ({ onToggle }) => {
     <div className="inline-flex p-0.5 sm:p-1 rounded-full bg-transparent outline outline-2 outline-gray-300 gap-1 sm:gap-2">
       <button
         className={`px-2 sm:px-4 py-1 sm:py-2 rounded-full transition-all duration-300 font-bold text-sm sm:text-xl ${
-          activeOption === 'Find'
-            ? 'bg-yellow-fig text-black-fig'
-            : 'bg-transparent text-yellow-fig hover:bg-gray-200'
-        }`}
-        onClick={() => handleToggle('Find')}
-      >
-        Find
-      </button>
-      <button
-        className={`px-2 sm:px-4 py-1 sm:py-2 rounded-full transition-all duration-300 font-bold text-sm sm:text-xl ${
           activeOption === 'Verify'
             ? 'bg-yellow-fig text-black-fig'
             : 'bg-transparent text-yellow-fig hover:bg-gray-200'
@@ -29,6 +19,16 @@ const ToggleButton = ({ onToggle }) => {
         onClick={() => handleToggle('Verify')}
       >
         Verify
+      </button>
+      <button
+        className={`px-2 sm:px-4 py-1 sm:py-2 rounded-full transition-all duration-300 font-bold text-sm sm:text-xl ${
+          activeOption === 'Find'
+            ? 'bg-yellow-fig text-black-fig'
+            : 'bg-transparent text-yellow-fig hover:bg-gray-200'
+        }`}
+        onClick={() => handleToggle('Find')}
+      >
+        Find
       </button>
     </div>
   );
