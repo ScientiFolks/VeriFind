4ce7fe63482254440229be514fcd3b44fde919ce
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-11-24 11:24:06 +0700

    feat: implement SearchService with search functionality

---
+++ AI/services/search.py
@@ -0,0 +1,27 @@
+from models import SearchResult, SearchAgent
+from configs import SearchConfig
+
+class SearchService:
+    def __init__(self) -> None:
+        self.search_agent = SearchAgent()
+        pass
+
+    async def search(
+        self,
+        query: str,
+        search_template: str = SearchConfig.SEARCH_TEMPLATE,
+        max_results: int = SearchConfig.DEFAULT_MAX_RESULTS,
+        include_domains = [],
+        exclude_domains = [],     
+    ) -> SearchResult:
+        """
+            Search the query and return the search results
+        """
+        print("service/search> SearchService::search")
+        return self.search_agent.invoke_search(
+            query=query,
+            search_template=search_template,
+            max_results=max_results,
+            include_domains=include_domains,
+            exclude_domains=exclude_domains,
+        )
