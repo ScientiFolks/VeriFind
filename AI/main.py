804d45cf0c89846107a58d0f6114ee464623b5c4
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-11-24 19:28:24 +0700

    feat: add extraction router to main application

--- AI/main.py
+++ AI/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import revision_router, search_router, validation_router
+from routes import revision_router, search_router, validation_router, extraction_router
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = FastAPI()
 
@@ -25,3 +29,5 @@ app.include_router(revision_router, prefix="/revision", tags=["revision"])
 app.include_router(search_router, prefix="/search", tags=["search"])
 # Include the validation routes
 app.include_router(validation_router, prefix="/validation", tags=["validation"])
+# Include the extraction routes
+app.include_router(extraction_router, prefix="/extraction", tags=["extraction"])
