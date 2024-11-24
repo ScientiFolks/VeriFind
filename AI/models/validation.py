108fff58f887bc7e9d85f7f7c6f9348a0b7d8dd7
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2024-11-24 18:29:17 +0700

    feat: create api

--- AI/models/validation.py
+++ AI/models/validation.py
@@ -16,12 +16,6 @@ from typing import List
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 
-import sys
-import os
-
-# Add the parent directory of 'services' to the path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from dotenv import load_dotenv
 from models import SearchAgent
 
@@ -119,12 +113,12 @@ class ValidationAgent:
                 x["intermediate_steps"]
             ),})
 
-    def summarize_text(document):
+    def summarize_text(self, document):
             chat = ChatGroq(temperature=0, model_name="llama3-8b-8192")
             system = """You are a helpful assistant. Your task is to review a document and provide a detailed summary of its content. 
                         Focus on capturing the main ideas, key points, and important details in a concise manner.
-                        Ensure the summary is no more than 50 words and remains true to the original intent and information of the document."""
-            human = """Summarize the following document in no more than 50 words, focusing on the main ideas and key points:
+                        Ensure the summary is no more than 5 words and remains true to the original intent and information of the document."""
+            human = """Summarize the following document in no more than 5 words, focusing on the main ideas and key points:
                     {document}"""
             prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
             
@@ -132,27 +126,21 @@ class ValidationAgent:
             return summarize_agent.invoke({"document": document})
 
     def validate_document(self, processed_pages : List[str]) -> dict:
-        batch_size = 1
-        num_batches = ceil(len(processed_pages) / batch_size)
         results = []
         searching_engine = SearchAgent()  
 
-        for batch_index in range(num_batches):
+        for batch_index in range(len(processed_pages)):
             # Slice the pages into a batch of three
-            start_index = batch_index * batch_size
-            end_index = start_index + batch_size
-            page_batch = processed_pages[start_index:end_index]
-            
-            # Combine the pages in the batch into one text block
-            batched_text = "\n".join(page_batch)
+            batched_text = processed_pages[batch_index]
 
             summary = self.summarize_text(batched_text).content
-
-            summary = summary.split("\n",1)[1]
+            print(summary)
+            
             search_query = summary + "Search for four titles of scholarly documents and the author/organization writing them most related to the summary above."
             search_result = searching_engine.invoke_search(search_query, max_results=4)
 
             urls = ""
+            print(search_result)
             
             for i, result in enumerate(search_result):
                 if (result.url != None) and (i != 0):
--- AI/services/validation.py
+++ AI/services/validation.py
@@ -28,14 +28,19 @@ class ValidationService:
         
         cleaned_output = re.sub(r"(Action:.*|Action Input:.*|Invalid Format:.*|Thought:.*)", "", last_iteration).strip()
 
-        final_output = self.remove_lines_before_substring(cleaned_output.replace("> Finished chain.", ""), "Comparison with Scholarly Works")
+        final_output = await self.remove_lines_before_substring(
+            cleaned_output.replace("> Finished chain.", ""), 
+            "Comparison with Scholarly Works"
+        )
         return final_output
+    
+
     
     async def validate_document(self, file: UploadFile):
         doc = await self.revision_service.load_document(file)
         processed_pages = []
 
-        for page_id in range (doc.page_count):
+        for page_id in range(doc.page_count):
             processed_pages.append(doc.load_page(page_id).get_text())
 
         responses = self.validation_agent.validate_document(processed_pages)
@@ -43,8 +48,8 @@ class ValidationService:
         suggestions = ""
 
         for response in responses:
-            suggestions += (f"\n\n Page {response['page_id']+1} \n  {response['suggestions']}")
-            response["suggestions"] = self.clean_output(response["suggestions"])
+            response["suggestions"] = await self.clean_output(response["suggestions"])
+            suggestions += f"\n\n Page {response['page_id']+1} \n  {response['suggestions']}"
 
         suggestion_summary = self.revision_agent.summary_suggestion(suggestions)
         
@@ -52,6 +57,7 @@ class ValidationService:
             "summary": suggestion_summary.content,
             "suggestions": responses
         }
+
     
     async def validate_statement(self, statement: str) -> str:
         response = self.validation_agent.validate_document([statement])
